13e92cb1dc9de218bfa711486d09f3324a007f6f
Louiza <123575767+1oui2a@users.noreply.github.com>
2024-10-06 20:05:27 +0000

    bootstrap navbar and routing

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,20 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NavigationBar from './components/NavigationBar';
 import Header from './components/Header';
-import Content from './components/Content';
 import Footer from './components/Footer';
+import Content from './components/Content';
 
 function App() {
   return (
-    <div>
-      <Header />
-      <Content />
+    <Router>
+      <NavigationBar />
+      <Routes>
+        <Route path="/home" element={<Content />} />
+        <Route path="/read" element={<h1>Read Component</h1>} />
+        <Route path="/create" element={<h1>Create Component</h1>} />
+      </Routes>
       <Footer />
-    </div>
+    </Router>
   );
 }
 
--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -1,5 +1,21 @@
-const Header = () => {
-    return <h1>My Header in another component</h1>;
-  };
-  
-  export default Header;
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+
+const NavigationBar = () => {
+  return (
+        <Navbar bg="dark" data-bs-theme="dark">
+          <Container>
+            <Navbar.Brand href="/">Navbar</Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/create">Create</Nav.Link>
+              <Nav.Link href="/read">Read</Nav.Link>
+            </Nav>
+          </Container>
+      </Navbar>
+  );
+};
+
+export default NavigationBar;
